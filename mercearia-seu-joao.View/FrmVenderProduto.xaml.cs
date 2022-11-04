961a539d9891ec5f689c4dfb8a6d23d1b40a1760
NicolyCdeJesus <nicoly.jesus@estudante.firjan.senai.br>
2022-11-04 14:39:58 -0300

    Criação de tela e classe Vender Produto

---
+++ mercearia-seu-joao.View/FrmVenderProduto.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mercearia_seu_joao.View
+{
+    /// <summary>
+    /// Interação lógica para FrmVenderProduto.xam
+    /// </summary>
+    public partial class FrmVenderProduto : Page
+    {
+        public FrmVenderProduto()
+        {
+            InitializeComponent();
+        }
+    }
+}
