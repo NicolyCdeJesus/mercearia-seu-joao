c4e8789c83c8862aa40f66d0bd9976256a816797
DeividRSilva <deivid.silva@estudante.firjan.senai.br>
2022-11-10 16:37:05 -0300

    Nova atualização

---
+++ mercearia-seu-joao.View/FrmGerenciarProduto.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mercearia_seu_joao.View
+{
+    /// <summary>
+    /// Lógica interna para FrmGerenciarProduto.xaml
+    /// </summary>
+    public partial class FrmGerenciarProduto : Window
+    {
+        List<Produto> listaDeProdutos = new List<Produto>();
+        public FrmGerenciarProduto()
+        {
+            InitializeComponent();
+            AtualizaDataGrid();
+
+        }
+
+        private void NovoProduto(object sender, RoutedEventArgs e)
+        {
+            if (VerificaCampos() == true)
+            {
+                if (txtID.Text == "")
+                {
+                    AdicionaProduto();
+                }
+                else
+                {
+                    //CaixaDeMenssagem.ExibirMenssagemIdCampoPreenchido();
+                }
+            }
+            else
+            {
+                //CaixaDeMenssagem.ExibirMenssagemPreencherCampos();
+            }
+
+        }
+
+        private void LimparCampos(object sender, RoutedEventArgs e)
+        {
+            LimpaTodosOsCampos();
+        }
+
+        private void AlterarProduto(object sender, RoutedEventArgs e)
+        {
+            if (txtID.Text != "")
+            {
+                int id = int.Parse(txtID.Text);
+                MessageBoxResult result = MessageBox.Show(
+                "Deseja alterar o produto id: {id} ?",
+                "Alterar o produto",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    bool foiAtualizado = ConsultasProduto.AtualizarProduto(
+                    id,
+                    txtNomeProduto.Text,
+                    txtFornecedor.Text,
+                    int.Parse(txtQuantidade.Text),
+                    float.Parse(txtPrecoUnitario.Text)
+                    );
+                    if (foiAtualizado == true)
+                    {
+                        //CaixaDeMenssagem.ExibirMenssagemProdutoAtualizado();
+                        LimpaTodosOsCampos();
+                        AtualizaDataGrid();
+                    }
+                    else
+                    {
+                        //CaixaDeMenssagem.ExibirMenssagemErroProdutoAtualizado();
+                    }
+                }
+            }
+        }
+
+        private void ExcluirProduto(object sender, RoutedEventArgs e)
+        {
+            if (txtID.Text != "")
+            {
+                int id = int.Parse(txtID.Text);
+                MessageBoxResult result = MessageBox.Show(
+                $"Deseja excluir o produto id:{id} ?",
+                "Excluir Produto",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    bool foiExcluido = ConsultasProduto.ExcluirProduto(id);
+                    if (foiExcluido == true)
+                    {
+                       // CaixaDeMenssagem.ExibirMenssagemProdutoExcluido();
+                        LimpaTodosOsCampos();
+                        AtualizaDataGrid();
+                    }
+                    else
+                    {
+                        //CaixaDeMenssagem.ExibirMenssagemErroProdutoExcluido();
+                    }
+                }
+            }
+
+        }
+
+        private bool VerificaCampos()
+        {
+            if (txtNomeProduto.Text != "" && txtFornecedor.Text != "" && txtQuantidade.Text != "" && txtPrecoUnitario.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void LimpaTodosOsCampos()
+        {
+            txtID.Text = "";
+            txtNomeProduto.Text = "";
+            txtFornecedor.Text = "";
+            txtQuantidade.Text = "";
+            txtPrecoUnitario.Text = "";
+        }
+        private void AdicionaProduto()
+        {
+            bool foiInserido = ConsultasProduto.InserirProduto(
+            txtNomeProduto.Text,
+            txtFornecedor.Text,
+            int.Parse(txtQuantidade.Text),
+            float.Parse(txtPrecoUnitario.Text)
+            );
+            if (foiInserido == true)
+            {
+               // CaixaDeMenssagem.ExibirMenssagemProdutoCadastrado();
+                LimpaTodosOsCampos();
+                AtualizaDataGrid();
+            }
+            else
+            {
+               // CaixaDeMenssagem.ExibirMenssagemErroProdutoCadastrado();
+            }
+        }
+        private void AtualizaDataGrid()
+        {
+            listaDeProdutos.Clear();
+            listaDeProdutos = ConsultasProduto.ObterTodosProdutos();
+            dgvProdutos.ItemsSource = listaDeProdutos;
+            dgvProdutos.Items.Refresh();
+        }
+        private void PegarItemNoGrid(object sender, MouseButtonEventArgs e)
+        {
+            Produto produto = (Produto)dgvProdutos.SelectedItem;
+            txtID.Text = produto.id.ToString();
+            txtNomeProduto.Text = produto.nome;
+            txtFornecedor.Text = produto.fornecedor;
+            txtQuantidade.Text = produto.qtd.ToString();
+            txtPrecoUnitario.Text = produto.qtd.ToString();
+        }
+    }
+}
---
+++ mercearia-seu-joao.View/GerenciarProduto.cs
@@ -0,0 +1,145 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ConsultasProduto
+{
+    //Cadastra o produto no banco de dados - CREATE
+    public static bool InserirProduto(string nome, string descricao, string fornecedor, float precounitario, int quantidade)
+    {
+        var conexao = new
+       MySqlConnection(ConexaoBD.Connection.ConnectionString);
+        bool foiInserido = false;
+        try
+        {
+            conexao.Open();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = @"
+            INSERT INTO Produto (nome,  qtd, fornecedor, precoUnitario) 
+            VALUES(@nome,@qtd,@fornecedor,@precoUnitario)";
+            comando.Parameters.AddWithValue("@nome", nome);
+            comando.Parameters.AddWithValue("@qtd", quantidade);
+            comando.Parameters.AddWithValue("@fornecedor", fornecedor);
+            comando.Parameters.AddWithValue("@precoUnitario", precounitario);
+            var leitura = comando.ExecuteReader();
+            foiInserido = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+        return foiInserido;
+    }
+    //Exclui um produto do banco de dados - REMOVE
+    public static bool ExcluirProduto(int id)
+    {
+        var conexao = new
+       MySqlConnection(ConexaoBD.Connection.ConnectionString);
+        bool foiExcluido = false;
+        try
+        {
+            conexao.Open();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = @"
+            DELETE FROM Produto WHERE id = @id";
+            comando.Parameters.AddWithValue("@id", id);
+            var leitura = comando.ExecuteReader();
+            foiExcluido = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+        return foiExcluido;
+    }
+    //Altera um produto no banco de dados - UPDATE
+    public static bool AtualizarProduto(int id, string nome, string fornecedor, int quantidade, float precounitario)
+    {
+        var conexao = new
+       MySqlConnection(ConexaoBD.Connection.ConnectionString);
+        bool foiAtualizado = false;
+        try
+        {
+            conexao.Open();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = @"
+            UPDATE Produto
+            SET nome = @nome, fornecedor = 
+            @fornecedor, qtd = @quantidade, precoUnitario = @precoUnitario,
+            WHERE id = @id";
+            comando.Parameters.AddWithValue("@id", id);
+            comando.Parameters.AddWithValue("@nome", nome);
+            comando.Parameters.AddWithValue("@quantidade", quantidade);
+            comando.Parameters.AddWithValue("@fabricante", fornecedor);
+            comando.Parameters.AddWithValue("@precoUnitario", precounitario);
+            var leitura = comando.ExecuteReader();
+            foiAtualizado = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+        return foiAtualizado;
+    }
+    //Retrona todos os produtos - READ
+    public static List<Produto> ObterTodosProdutos()
+    {
+        var conexao = new
+       MySqlConnection(ConexaoBD.Connection.ConnectionString);
+        List<Produto> listaDeProdutos = new List<Produto>();
+        try
+        {
+            conexao.Open();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = @"
+            SELECT * FROM Produto;";
+            var leitura = comando.ExecuteReader();
+            while (leitura.Read())
+            {
+                Produto produto = new Produto();
+                produto.id = leitura.GetInt32("id");
+                produto.nome = leitura.GetString("nome");
+                produto.fornecedor = leitura.GetString("fornecedor");
+                produto.qtd = leitura.GetInt32("qtd");
+                produto.precoUnitario = leitura.GetFloat("@precoUnitario");
+                listaDeProdutos.Add(produto);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+        return listaDeProdutos;
+    }
+}
