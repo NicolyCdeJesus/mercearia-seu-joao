afb8e77ab8ce173fb96eb74630ccdc1fdf19f8ef
DeividRSilva <deivid.silva@estudante.firjan.senai.br>
2022-11-16 14:09:04 -0300

    Nova atualização.1

--- mercearia-seu-joao.Model/CaixaDeMensagem.cs
+++ mercearia-seu-joao.Model/CaixaDeMensagem.cs
@@ -74,66 +74,59 @@ namespace mercearia_seu_joao.Model
                                MessageBoxImage.Error);
         }
 
-        /*public static void ExibirMenssagemIdCampoPreenchido()
-        {
-            MessageBoxResult result2 = MessageBox.Show(
-                "Limpe o campo Id para poder inserir!",
-                "Atenção",
-                MessageBoxButton.OK,
-                MessageBoxImage.Warning);
-        }
-        public static void ExibirMen
+        /*public static void ExibirMen
             MessageBoxButton.OK,
-            MessageBoxImage.Warning);
-    }
-    public static void ExibirMenssagemProdutoCadastrado()
-    {
+           MessageBoxImage.Warning);*/
+    
+        public static void ExibirMenssagemProdutoCadastrado()
+        {
         MessageBoxResult result2 = MessageBox.Show(
             "Produto foi cadastrado!",
             "Atenção",
             MessageBoxButton.OK,
             MessageBoxImage.Warning);
-    }
-    public static void ExibirMenssagemErroProdutoCadastrado()
-    {
-        MessageBoxResult result2 = MessageBox.Show(
+        }
+        public static void ExibirMenssagemErroProdutoCadastrado()
+        {
+            MessageBoxResult result2 = MessageBox.Show(
                            "Não foi possível inserir o Produto, tente novamente mais tarde!",
                            "Atenção",
                            MessageBoxButton.OK,
                            MessageBoxImage.Error);
-    }
-    public static void ExibirMenssagemErroProdutoAtualizado()
-    {
-        MessageBoxResult result2 = MessageBox.Show(
+        }
+        public static void ExibirMenssagemErroProdutoAtualizado()
+        {
+            MessageBoxResult result2 = MessageBox.Show(
                            "Não foi possível atualizar o Produto, tente novamente mais tarde!",
                            "Atenção",
                            MessageBoxButton.OK,
                            MessageBoxImage.Error);
-    }
-    public static void ExibirMenssagemProdutoAtualizado()
-    {
-        MessageBoxResult result2 = MessageBox.Show(
+        }
+        public static void ExibirMenssagemProdutoAtualizado()
+        {
+            MessageBoxResult result2 = MessageBox.Show(
                            "Produto foi atualizado!",
                            "Informação",
                            MessageBoxButton.OK,
                            MessageBoxImage.Information);
-    }
-    public static void ExibirMenssagemProdutoExcluido()
-    {
-        MessageBoxResult result2 = MessageBox.Show(
+        }
+        public static void ExibirMenssagemProdutoExcluido()
+        {
+            MessageBoxResult result2 = MessageBox.Show(
                            "Produto foi excluido!",
                            "Informação",
                            MessageBoxButton.OK,
                            MessageBoxImage.Information);
-    }
-    public static void ExibirMenssagemErroProdutoExcluido()
-    {
-        MessageBoxResult result2 = MessageBox.Show(
+        }
+        public static void ExibirMenssagemErroProdutoExcluido()
+        {
+            MessageBoxResult result2 = MessageBox.Show(
                            "Não foi possível excluir o produto",
                            "Error",
                            MessageBoxButton.OK,
                            MessageBoxImage.Error);
+        }
+    
     }
-    */
-    }
+    
 }
